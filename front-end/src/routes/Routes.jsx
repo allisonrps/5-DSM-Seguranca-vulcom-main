bc1e489051bd868eb18c5f2cd5e58aa539b1d5b6
Allison Rodrigues <allison_rps@hotmail.com>
2025-05-23 22:58:40 -0300

    (23/05) Término da organização das rotas no front-end

--- front-end/src/routes/Routes.jsx
+++ front-end/src/routes/Routes.jsx
@@ -1,8 +1,7 @@
 /*
-Define as rotas e suas informações, servido como fonte única de
-verdade para AppRoutes.jsx e MainMenu.jsx
+  Define as rotas e suas informações, servido como fonte única de
+  verdade para AppRoutes.jsx e MainMenu.jsx
 */
-
 import Homepage from '../pages/Homepage'
 
 import CarForm from '../pages/car/CarForm'
@@ -15,86 +14,96 @@ import UserList from '../pages/user/UserList'
 import UserForm from '../pages/user/UserForm'
 
 import Login from '../pages/Login'
+
 import BruteForce from '../pages/BruteForce'
 
-//Constantes que definem o nivel de acesso
-const NO_USER = 0              // não ha necessidade do usuário autenticar
-const AUTHENTICATED_USER = 1   // somente usuário autentcados
-const ADMIN_USER = 2           // somente usuários administradores
+// Constantes que definem o nível de acesso do usuário
+const NO_USER = 0             // Não há necessidade de usuário autenticado
+const AUTHENTICATED_USER = 1  // Somente usuários autenticados
+const ADMIN_USER = 2          // Somente usuários administradores
 
 const routes = [
-    {
-        path: '/',
-        description: "Inicio",
-        element: <Homepage />,
-        userLevel: NO_USER,
-        divider: true           // mostra uma divisória no menu
-    },
-        {
-        path: '/login',
-        description: "Entrar",
-        element: <Login />,
-        userLevel: NO_USER,
-        omitFromMainMenu: true   // não aparece no menu principal
-        },
-        {
-        path: '/costumers',
-        description: "Listagem de Clientes",
-        element: <CostumerList />,
-        userLevel: AUTHENTICATED_USER,
-        },
-        {
-        path: '/costumers/new',
-        description: "Cadastro de Clientes",
-        element: <CostumerForm />,
-        userLevel: AUTHENTICATED_USER,
-        omitFromMainMenu: true
-    },
-        {
-        path: '/cars',
-        description: "Listagem de Veículos",
-        element: <CarList />,
-        userLevel: AUTHENTICATED_USER,
-    },
-        {
-        path: '/cars/new',
-        description: "Cadastro de Veículos",
-        element: <CarForm />,
-        userLevel: AUTHENTICATED_USER,
-    },
-        {
-        path: '/cars/:id',
-        description: "Alterar Veículos",
-        element: <CarForm />,
-        userLevel: AUTHENTICATED_USER,
-        omitFromMainMenu: true
-    },
-            {
-        path: '/users',
-        description: "Listagem de Usuários",
-        element: <UserList />,
-        userLevel: ADMIN_USER,
-    },
-                {
-        path: '/users/new',
-        description: "Cadastro de Usuários",
-        element: <UserForm />,
-        userLevel: ADMIN_USER,
-        divider: true
-    },
-                {
-        path: '/users/:id',
-        description: "Alterar Usuários",
-        element: <UserForm />,
-        userLevel: ADMIN_USER,
-    },
-                {
-        path: '/brute-force',
-        description: "Ataque de Força Bruta",
-        element: <BruteForce />,
-        userLevel: ADMIN_USER,
-        divider: true
-    },
+  {
+    path: '/',
+    description: 'Início',
+    element: <Homepage />,
+    userLevel: NO_USER,
+    divider: true           // Mostra uma divisória no menu
+  },
+  {
+    path: '/login',
+    description: 'Entrar',
+    element: <Login />,
+    userLevel: NO_USER,
+    omitFromMainMenu: true  // Não aparece no menu principal
+  },
+  {
+    path: '/customers',
+    description: 'Listagem de clientes',
+    element: <CustomerList />,
+    userLevel: AUTHENTICATED_USER
+  },
+  {
+    path: '/customers/new',
+    description: 'Cadastro de clientes',
+    element: <CustomerForm />,
+    userLevel: AUTHENTICATED_USER,
+    divider: true
+  },
+  {
+    path: '/customers/:id',
+    description: 'Alterar cliente',
+    element: <CustomerForm />,
+    userLevel: AUTHENTICATED_USER,
+    omitFromMainMenu: true
+  },
+  {
+    path: '/cars',
+    description: 'Listagem de veículos',
+    element: <CarList />,
+    userLevel: AUTHENTICATED_USER
+  },
+  {
+    path: '/cars/new',
+    description: 'Cadastro de veículos',
+    element: <CarForm />,
+    userLevel: AUTHENTICATED_USER,
+    divider: true
+  },
+  {
+    path: '/cars/:id',
+    description: 'Alterar veículo',
+    element: <CarForm />,
+    userLevel: AUTHENTICATED_USER,
+    omitFromMainMenu: true
+  },
+  {
+    path: '/users',
+    description: 'Listagem de usuários',
+    element: <UserList />,
+    userLevel: ADMIN_USER
+  },
+  {
+    path: '/users/new',
+    description: 'Cadastro de usuários',
+    element: <UserForm />,
+    userLevel: ADMIN_USER,
+    divider: true
+  },
+  {
+    path: '/users/:id',
+    description: 'Alterar usuário',
+    element: <UserForm />,
+    userLevel: ADMIN_USER,
+    omitFromMainMenu: true
+  },
+  {
+    path: '/brute-force',
+    description: 'Ataque de força bruta',
+    element: <BruteForce />,
+    userLevel: ADMIN_USER,
+    divider: true
+  }
 ]
 
-export { routes, NO_USER, AUTHENTICATED_USER, ADMIN_USER}
+export { routes, NO_USER, AUTHENTICATED_USER, ADMIN_USER }
